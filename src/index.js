bcdb63627c428957e6799f234c52bb161d0827de
Pushpendra Singh <pushpendra7060@gmail.com>
2020-08-10 17:12:27 +0530

    directry structure is created and store is working fine

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import { configureStore } from './store';
+
+const store = configureStore();
+
+console.log('Your current state is', store.getState())
+
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/reducers/search.js
@@ -0,0 +1,3 @@
+export default function search(state = {}, action) {
+  return state;
+}
